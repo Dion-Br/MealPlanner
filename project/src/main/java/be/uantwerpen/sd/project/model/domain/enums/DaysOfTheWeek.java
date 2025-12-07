f1c4812fbd8adb764d352d8d93d631f9b04f55cd
Dion Br <dion_brovina@hotmail.com>
2025-12-07 21:29:49 +0100

    Enums filled

--- project/src/main/java/be/uantwerpen/sd/project/model/domain/enums/DaysOfTheWeek.java
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/enums/DaysOfTheWeek.java
@@ -1,4 +1,8 @@
 package be.uantwerpen.sd.project.model.domain.enums;
 
 public enum DaysOfTheWeek {
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SNACK
 }
--- project/src/main/java/be/uantwerpen/sd/project/model/domain/enums/MealType.java
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/enums/MealType.java
@@ -1,4 +1,11 @@
 package be.uantwerpen.sd.project.model.domain.enums;
 
 public enum MealType {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
 }
