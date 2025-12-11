1afde177a7db2a5cd3d9daa349c1421e8be97e1e
amal laassikri <amal.laassikri@student.kdg.be>
2025-12-11 15:19:26 +0100

    fixed prop change thing

--- project/src/main/java/be/uantwerpen/sd/project/model/domain/WeeklyMealPlan.java
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/WeeklyMealPlan.java
@@ -7,7 +7,7 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WeeklyMealPlan implements PropertyChangeListener {
+public class WeeklyMealPlan{
     private List<DayPlan> dayPlans;
 
     public WeeklyMealPlan() {
@@ -16,12 +16,6 @@ public class WeeklyMealPlan implements PropertyChangeListener {
             dayPlans.add(new DayPlan(day));
         }
     }
-
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-
-    }
-
     public List<DayPlan> getDayPlans() {
         return dayPlans;
     }
