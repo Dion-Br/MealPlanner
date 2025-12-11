6cac36ce7e49aa385777392e3f5af933f6ff5b2c
Dion Br <dion_brovina@hotmail.com>
2025-12-11 14:59:13 +0100

    DayPlan and PlannedMeal classes

---
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/DayPlan.java
@@ -0,0 +1,9 @@
+package be.uantwerpen.sd.project.model.domain;
+
+import be.uantwerpen.sd.project.model.domain.enums.DaysOfTheWeek;
+import java.util.*;
+
+public class DayPlan {
+    private DaysOfTheWeek day;
+    private List<PlannedMeal> plannedMeals;
+}
---
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/PlannedMeal.java
@@ -0,0 +1,9 @@
+package be.uantwerpen.sd.project.model.domain;
+
+import be.uantwerpen.sd.project.model.domain.enums.DaysOfTheWeek;
+import be.uantwerpen.sd.project.model.domain.enums.MealType;
+
+public class PlannedMeal {
+    private MealType mealType;
+    private MealComponent mealComponent;
+}
