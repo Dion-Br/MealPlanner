c12f9970a125869a38001227769d288328ef9cec
Dion Br <dion_brovina@hotmail.com>
2025-12-11 15:06:05 +0100

    update Dayplan and PlannedMeal functions

--- project/src/main/java/be/uantwerpen/sd/project/model/domain/DayPlan.java
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/DayPlan.java
@@ -6,4 +6,18 @@ import java.util.*;
 public class DayPlan {
     private DaysOfTheWeek day;
     private List<PlannedMeal> plannedMeals;
+
+    public DayPlan(DaysOfTheWeek day, List<PlannedMeal> plannedMeals) {
+        this.day = day;
+        this.plannedMeals = plannedMeals;
+    }
+
+    public void addPlannedMeal(PlannedMeal newMeal){
+        this.plannedMeals.add(newMeal);
+    }
+
+    // Eventueel als we een verschuif functie in de agenda willen gebruiken
+    public void changeDayOfTheWeek(DaysOfTheWeek newDay){
+        this.day = newDay;
+    }
 }
--- project/src/main/java/be/uantwerpen/sd/project/model/domain/PlannedMeal.java
+++ project/src/main/java/be/uantwerpen/sd/project/model/domain/PlannedMeal.java
@@ -6,4 +6,20 @@ import be.uantwerpen.sd.project.model.domain.enums.MealType;
 public class PlannedMeal {
     private MealType mealType;
     private MealComponent mealComponent;
+
+    public PlannedMeal(MealType mealType, MealComponent mealComponent) {
+        this.mealType = mealType;
+        this.mealComponent = mealComponent;
+    }
+
+    // Maaltijd veranderen van Breakfast naar Lunch bv.
+    public void changeMealType(MealType newMealType){
+        this.mealType = newMealType;
+    }
+
+    // De volledige maaltijd vervangen door iets anders..
+    // Wordt waarschijnlijk nooit gebruikt
+    public void changeMealComponent(MealComponent newMealComponent){
+        this.mealComponent = newMealComponent;
+    }
 }
